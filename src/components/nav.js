00330be94fa5da78e508698e8fe447df899b9c48
VictorIzquier2 <victor.izquierdo1985@gmail.com>
2021-02-28 23:55:41 +0100

    nosotros

--- src/components/nav.js
+++ src/components/nav.js
@@ -1,12 +1,32 @@
 import React from 'react';
 import {Link} from 'gatsby';
-import {css} from '@emotion/react';
+import styled from '@emotion/styled';
+
+const Nav = styled.nav`
+  display: flex;
+  justify-content: center;
+  padding-bottom: 3rem;
+  @media(min-width: 768px) {
+    padding-bottom: 0;
+  }
+`;
+
+const NavLink = styled(Link)`
+  color: #fff;
+  font-size: 1.6rem;
+  font-weight: 700;
+  line-height: 1;
+  font-family: 'PT Sans', sans-serif;
+  text-decoration: none;
+  padding: 1rem;
+`;
 
 const Navegacion = () => {
   return (
-    <nav>
-      <Link to={'/'}>Inicio</Link>
-    </nav>
+    <Nav>
+      <NavLink to={'/'}>Inicio</NavLink>
+      <NavLink to={'/nosotros'}>Nosotros</NavLink>
+    </Nav>
   )
 }
 export default Navegacion;
